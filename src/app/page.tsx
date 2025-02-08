727d4709b6dcf476e0b2af5dc74961b00bae4538
Haesung <koh1260@naver.com>
2025-02-08 18:43:13 +0900

    refactor: 소셜 로그인 버튼 컴포넌트 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import Card from "@/components/Card";
 import Logo from "@/components/Logo";
+import OauthButton from "@/components/OauthButton";
 import { useGoogleLogin } from "@react-oauth/google";
 
 export const Home = () => {
@@ -10,21 +11,12 @@ export const Home = () => {
 
   return (
     <main className="flex justify-center items-center w-screen h-screen bg-center bg-cover bg-homeBg opacity-80">
-      <Card className="flex flex-col bg-slate-300 gap-6 p-8">
+      <Card className="flex flex-col bg-slate-400 gap-6 p-8">
         <Logo />
         <div className="flex flex-col gap-4">
-          <button
-            className="w-72 h-11 bg-white rounded-xl p-1 hover:bg-gray-100"
-            onClick={() => googleLogin()}
-          >
-            <p className="text-base">구글로 계속하기</p>
-          </button>
-          <button className="w-72 h-11 bg-kakao rounded-xl p-1 hover:bg-kakaoHover">
-            <p className="text-base text-kakaoText">카카오로 계속하기</p>
-          </button>
-          <button className="w-72 h-11 bg-naver rounded-xl p-1 hover:bg-naverHover">
-            <p className="text-base text-white">네이버로 계속하기</p>
-          </button>
+          <OauthButton type={"GOOGLE"} onClick={googleLogin} />
+          <OauthButton type={"KAKAO"} onClick={() => {}} />
+          <OauthButton type={"NAVER"} onClick={() => {}} />
           <button className="mt-3">
             <p className="text-sm text-stone-500 underline">
               비회원으로 시작하기
