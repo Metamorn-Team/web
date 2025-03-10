22746f20813dcb23c5d31c1ab008dc82528435ee
Haesung <koh1260@naver.com>
2025-03-10 21:01:22 +0900

    chore: 기사 에셋 추가 (테스트용)

--- src/game/entities/Player.ts
+++ src/game/entities/Player.ts
@@ -1,19 +1,26 @@
 import * as Phaser from "phaser";
 
 export class Player extends Phaser.Physics.Matter.Sprite {
-  private speed = 2;
+  private speed = 6;
   private cursor?: Phaser.Types.Input.Keyboard.CursorKeys;
 
   constructor(scene: Phaser.Scene, x: number, y: number) {
-    super(scene.matter.world, x, y, 'human-base-idle');
+    super(scene.matter.world, x, y, "warrior");
     scene.add.existing(this);
 
-    this.setRectangle(12, 17);
+    this.setRectangle(96, 96);
     this.setFixedRotation();
 
+    this.on(Phaser.Animations.Events.ANIMATION_COMPLETE, (animation) => {
+      console.log(animation);
+      if (animation.key === "warrior-attack") {
+        this.play("warrior-idle");
+      }
+    });
+
     this.cursor = scene.input.keyboard?.createCursorKeys();
-    
-    this.play('human-idle');
+
+    this.play("warrior-idle");
   }
 
   update(): void {
@@ -22,23 +29,34 @@ export class Player extends Phaser.Physics.Matter.Sprite {
 
     if (this.cursor?.left.isDown) {
       velocityX = -this.speed;
+      this.play("warrior-walk", true);
       this.setFlipX(true);
     }
     if (this.cursor?.right.isDown) {
       velocityX = this.speed;
+      this.play("warrior-walk", true);
       this.setFlipX(false);
     }
     if (this.cursor?.up.isDown) {
+      this.play("warrior-walk", true);
       velocityY = -this.speed;
     }
     if (this.cursor?.down.isDown) {
+      this.play("warrior-walk", true);
       velocityY = this.speed;
+    }
+    if (this.cursor?.space.isDown) {
+      this.play("warrior-attack", true);
     }
 
     this.setVelocity(velocityX, velocityY);
 
+    if (this.anims.currentAnim?.key === "warrior-attack") {
+      return;
+    }
+
     if (velocityX === 0 && velocityY === 0) {
-      this.play("human-idle", true);
+      this.play("warrior-idle", true);
     }
   }
 }
--- src/game/scenes/MainScene.ts
+++ src/game/scenes/MainScene.ts
@@ -9,11 +9,17 @@ export class MainScene extends Phaser.Scene {
   }
 
   preload() {
+    const warirrorBlue = "warrior_blue.png";
+    const warirrorPurple = "warrior_purple.png";
     this.load.audio("town", "/sounds/town.mp3");
     this.load.image("background", "/images/background.png");
     this.load.spritesheet("human-base-idle", "/game/human/idle/base_idle.png", {
       frameWidth: 96,
       frameHeight: 64,
+    });
+    this.load.spritesheet("warrior", `/game/warrior/${warirrorPurple}`, {
+      frameWidth: 192,
+      frameHeight: 192,
     });
     this.load.image(
       "sunnysideworld_16px",
@@ -39,7 +45,7 @@ export class MainScene extends Phaser.Scene {
     this.player = new Player(this, mapWidth / 2, mapHeight / 2);
 
     this.cameras.main.setBounds(0, 0, mapWidth, mapHeight);
-    this.cameras.main.setZoom(2.2);
+    this.cameras.main.setZoom(0.9);
     this.followPlayerCamera();
     this.defineAnimation();
   }
@@ -62,6 +68,35 @@ export class MainScene extends Phaser.Scene {
       }),
       frameRate: 15,
       repeat: -1,
+    });
+
+    this.anims.create({
+      key: "warrior-idle",
+      frames: this.anims.generateFrameNumbers("warrior", {
+        start: 0,
+        end: 5,
+      }),
+      frameRate: 15,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "warrior-walk",
+      frames: this.anims.generateFrameNumbers("warrior", {
+        start: 6,
+        end: 11,
+      }),
+      frameRate: 15,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "warrior-attack",
+      frames: this.anims.generateFrameNumbers("warrior", {
+        start: 12,
+        end: 17,
+      }),
+      frameRate: 15,
     });
   }
 }
