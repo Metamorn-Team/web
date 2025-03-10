f4ebbbd73a7a8460c50ccedde87dd57dbe153532
Haesung <koh1260@naver.com>
2025-03-10 20:32:28 +0900

    chore: 배경 음악 볼륨 다운

--- src/game/scenes/MainScene.ts
+++ src/game/scenes/MainScene.ts
@@ -9,33 +9,35 @@ export class MainScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.audio('town', '/sounds/town.mp3');
-    this.load.image('background', '/images/background.png');
-    this.load.spritesheet(
-      "human-base-idle",
-      "/game/human/idle/base_idle.png",
-      {
-        frameWidth: 96,
-        frameHeight: 64,
-      }
+    this.load.audio("town", "/sounds/town.mp3");
+    this.load.image("background", "/images/background.png");
+    this.load.spritesheet("human-base-idle", "/game/human/idle/base_idle.png", {
+      frameWidth: 96,
+      frameHeight: 64,
+    });
+    this.load.image(
+      "sunnysideworld_16px",
+      "/game/tiles/sunnysideworld_16px.png"
     );
-    this.load.image('sunnysideworld_16px', '/game/tiles/sunnysideworld_16px.png');
-    this.load.tilemapTiledJSON('town', '/game/maps/town.json');
+    this.load.tilemapTiledJSON("town", "/game/maps/town.json");
   }
 
   create() {
-    const map = this.make.tilemap({ key: 'town' });
-    const tileset = map.addTilesetImage('sunnysideworld_16px', 'sunnysideworld_16px');
-    const groundLayer = map.createLayer('ground', tileset!);
+    const map = this.make.tilemap({ key: "town" });
+    const tileset = map.addTilesetImage(
+      "sunnysideworld_16px",
+      "sunnysideworld_16px"
+    );
+    const groundLayer = map.createLayer("ground", tileset!);
     groundLayer?.setAlpha(1);
 
     const mapWidth = map.widthInPixels;
     const mapHeight = map.heightInPixels;
     this.matter.world.setBounds(0, 0, mapWidth, mapHeight);
-    this.sound.play('town');
+    this.sound.play("town", { volume: 0.3 });
 
     this.player = new Player(this, mapWidth / 2, mapHeight / 2);
-    
+
     this.cameras.main.setBounds(0, 0, mapWidth, mapHeight);
     this.cameras.main.setZoom(2.2);
     this.followPlayerCamera();
