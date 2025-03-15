71e552c23a520630d89b158d8c7cd6b6da3cb9bb
Haesung <koh1260@naver.com>
2025-03-16 03:49:36 +0900

    feat: 홈 화면에 phaser 화면 설정

--- app/home/home.tsx
+++ app/home/home.tsx
@@ -1,3 +1,30 @@
+import { initializeGame } from "game/main";
+import { useEffect, useRef } from "react";
+
 export default function Home() {
-  return <div>Home</div>;
+  const gameContainer = useRef(null);
+
+  useEffect(() => {
+    const game = initializeGame(window.innerWidth, window.innerHeight);
+
+    const handleResize = () => {
+      game.scale.resize(window.innerWidth, window.innerHeight);
+      game.scene.scenes.forEach((scene) => {
+        scene.cameras.main.setSize(window.innerWidth, window.innerHeight);
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      game.destroy(true);
+    };
+  });
+
+  return (
+    <div>
+      <div ref={gameContainer} id="game-container" className=" z-50" />
+    </div>
+  );
 }
---
+++ game/main.ts
@@ -0,0 +1,18 @@
+import { MainScene } from "game/scenes/main-scene";
+import * as Phaser from "phaser";
+
+export const initializeGame = (width: number, height: number) => {
+  return new Phaser.Game({
+    type: Phaser.AUTO,
+    width,
+    height,
+    scene: MainScene,
+    physics: {
+      default: "matter",
+      matter: {
+        gravity: { x: 0, y: 0 },
+        debug: true,
+      },
+    },
+  });
+};
