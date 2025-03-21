6f3637962ae2019426b34804ffffe77449218862
Haesung <koh1260@naver.com>
2025-03-21 20:26:29 +0900

    feat: 광장 페이지 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,9 @@
 "use client";
-import { useEffect, useRef } from "react";
 
 const MainPage = () => {
-  const gameContainer = useRef(null);
-
-  useEffect(() => {
-    import("@/game/main").then((mod) => {
-      const initializeGame = mod.initializeGame;
-      const game = initializeGame(window.innerWidth, window.innerHeight);
-
-      const handleResize = () => {
-        game.scale.resize(window.innerWidth, window.innerHeight);
-        game.scene.scenes.forEach((scene) => {
-          scene.cameras.main.setSize(window.innerWidth, window.innerHeight);
-        });
-      };
-      window.addEventListener("resize", handleResize);
-
-      return () => {
-        window.removeEventListener("resize", handleResize);
-        game.destroy(true);
-      };
-    });
-  }, []);
-
   return (
     <div>
-      <div ref={gameContainer} id="game-container" className="z-0"></div>
-      <button className="absolute left-1/2 bg-white round">음소거</button>
+      <h1>메인 페이지</h1>
     </div>
   );
 };
