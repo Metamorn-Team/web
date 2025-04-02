53d8fc84be74e29fc284a3b338088b3e41909cff
Haesung <koh1260@naver.com>
2025-04-03 04:35:07 +0900

    chore: 광산 히트박스 크기 조정

--- src/game/objects/mine.ts
+++ src/game/objects/mine.ts
@@ -2,7 +2,7 @@ export class Mine extends Phaser.Physics.Matter.Sprite {
   constructor(scene: Phaser.Scene, x: number, y: number) {
     super(scene.matter.world, x, y, "mine");
     scene.add.existing(this);
-    this.setRectangle(150, 100);
+    this.setRectangle(150, 80);
     this.setStatic(true);
   }
 }
