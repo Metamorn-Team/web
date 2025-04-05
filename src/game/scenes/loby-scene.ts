a2d761d51a9572414330fbee9db6fe9c1d0d67e7
Haesung <koh1260@naver.com>
2025-04-06 04:47:09 +0900

    refactor: 로비 scene에서 소켓 관련 코드 제거

--- src/game/scenes/loby-scene.ts
+++ src/game/scenes/loby-scene.ts
@@ -1,18 +1,14 @@
 import { TorchGoblin } from "@/game/entities/npc/torch-goblin";
 import { Pawn } from "@/game/entities/players/pawn";
-import { Player } from "@/game/entities/players/player";
 import { EventBus } from "@/game/event/EventBus";
 import { socketManager } from "@/game/managers/socket-manager";
-import { spawnManager } from "@/game/managers/spawn-manager";
 import { Mine } from "@/game/objects/mine";
 import { Phaser } from "@/game/phaser";
 import { MetamornScene } from "@/game/scenes/meramorn-scene";
-import { ClientToServerEvents, ServerToClientEvents } from "@/types/socket-io";
+import { getItem } from "@/utils/persistence";
 import { setItem } from "@/utils/session-storage";
-import { Socket } from "socket.io-client";
 
 export class LobyScene extends MetamornScene {
-  private otherPlayers: { [playerId: string]: Player } = {};
   private npcGoblin: TorchGoblin;
   private mine: Mine;
 
@@ -22,11 +18,8 @@ export class LobyScene extends MetamornScene {
   private mapHeight: number;
   private centerOfMap: { x: number; y: number };
 
-  private isEnterPortal: boolean = false;
-
   public updateLoadingState: (state: boolean) => void;
 
-  private io: Socket<ServerToClientEvents, ClientToServerEvents>;
   private socketNsp = "loby";
 
   constructor() {
@@ -38,13 +31,7 @@ export class LobyScene extends MetamornScene {
   create() {
     this.initWorld();
 
-    this.io = socketManager.connect(this.socketNsp)!;
     this.spwanMyPlayer();
-
-    this.io.emit("playerJoin", {
-      x: this.centerOfMap.x,
-      y: this.centerOfMap.y,
-    });
 
     this.npcGoblin = new TorchGoblin(
       this,
@@ -52,31 +39,18 @@ export class LobyScene extends MetamornScene {
       this.centerOfMap.y,
       "red"
     );
-    this.mine = new Mine(this, this.centerOfMap.x, this.centerOfMap.y - 200);
+    this.mine = new Mine(
+      this,
+      this.centerOfMap.x,
+      this.centerOfMap.y - 200,
+      "dev"
+    );
 
     this.listenEvents();
   }
 
   update(): void {
     this.player.update();
-    Object.values(this.otherPlayers).forEach((player) => {
-      player.update();
-    });
-
-    const distance = Phaser.Math.Distance.Between(
-      this.mine.x,
-      this.mine.y + 40,
-      this.player.x,
-      this.player.y
-    );
-    if (distance < 30 && !this.isEnterPortal) {
-      EventBus.emit("in-portal", { category: "개발" });
-      this.isEnterPortal = true;
-    }
-    if (distance > 29 && this.isEnterPortal) {
-      EventBus.emit("out-portal");
-      this.isEnterPortal = false;
-    }
   }
 
   createTileMapLayers(map: Phaser.Tilemaps.Tilemap) {
@@ -100,65 +74,7 @@ export class LobyScene extends MetamornScene {
     ]);
   }
 
-  setPortal() {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    // this.matter.world.on("collisionstart", (_: any, bodyA: any, bodyB: any) => {
-    //   console.log(_);
-    //   if (
-    //     bodyA.gameObject instanceof Player &&
-    //     bodyB.gameObject instanceof Mine
-    //   ) {
-    //     EventBus.emit("portal", { category: "개발" });
-    //   }
-    //   if (
-    //     bodyA.gameObject instanceof Sheep &&
-    //     bodyB.gameObject instanceof Mine
-    //   ) {
-    //     bodyA.gameObject.destroy();
-    //   }
-    // });
-  }
-
-  handlePlayerMove(playerId: string, x: number, y: number) {
-    const player = this.otherPlayers[playerId];
-    if (player) {
-      const dx = this.otherPlayers[playerId].x - x;
-      const dy = this.otherPlayers[playerId].y - y;
-
-      if (dx > 0) {
-        player.walk("left");
-      } else if (dx < 0) {
-        player.walk("right");
-      } else if (dy > 0) {
-        player.walk("up");
-      } else if (dy < 0) {
-        player.walk("down");
-      } else {
-        player.idle();
-      }
-
-      player.targetPosition.x = x;
-      player.targetPosition.y = y;
-    }
-  }
-
-  spawnActiveUsers(activeUsers: {
-    [playerId: string]: { x: number; y: number };
-  }) {
-    Object.keys(activeUsers).forEach((playerId) =>
-      spawnManager.spawnPlayer(
-        this.otherPlayers,
-        this,
-        playerId,
-        activeUsers[playerId].x,
-        activeUsers[playerId].y
-      )
-    );
-  }
-
   initWorld() {
-    this.setPortal();
-
     const map = this.make.tilemap({ key: "home" });
     this.createTileMapLayers(map);
 
@@ -185,61 +101,35 @@ export class LobyScene extends MetamornScene {
   }
 
   spwanMyPlayer() {
+    const player = getItem("profile");
+
     this.player = new Pawn(
       this,
       this.centerOfMap.x,
       this.centerOfMap.y,
       "red",
-      "Snail",
-      true,
-      this.io
+      player ? player : { id: "", tag: "", nickname: "" },
+      true
     );
     this.followPlayerCamera();
   }
 
+  setBgmPlay(state: boolean) {
+    this.sound.setVolume(state ? 0.15 : 0);
+    this.isMute = state;
+  }
+
   listenEvents() {
-    this.io.on("activeUsers", (activeUsers) => {
-      console.log(activeUsers);
-      this.spawnActiveUsers(activeUsers);
-    });
-
-    this.io.on("playerJoin", (data) => {
-      const playerId = data.playerId as string;
-      spawnManager.spawnPlayer(
-        this.otherPlayers,
-        this,
-        playerId,
-        this.centerOfMap.x,
-        this.centerOfMap.y
-      );
-    });
-
-    this.io.on("playerLeft", (data) => {
-      const playerId = data.playerId as string;
-      this.otherPlayers[playerId].destroy();
-      delete this.otherPlayers[playerId];
-    });
-
-    this.io.on("playerMoved", (data) => {
-      this.handlePlayerMove(data.playerId, data.x, data.y);
-    });
-
-    EventBus.on("move-to-zone", () => {
+    EventBus.on("join-zone", (data: { type: "dev" | "design" }) => {
       this.cameras.main.fadeOut(500);
       socketManager.disconnect(this.socketNsp);
 
       this.time.delayedCall(500, () => {
         this.sound.stopAll();
         EventBus.emit("start-change-scene");
-        const type = "dev";
-        this.scene.start("ZoneScene", { type });
-        setItem("zone_type", type);
+        this.scene.start("ZoneScene", data);
+        setItem("zone_type", data.type);
       });
     });
-  }
-
-  setBgmPlay(state: boolean) {
-    this.sound.setVolume(state ? 0.15 : 0);
-    this.isMute = state;
   }
 }
