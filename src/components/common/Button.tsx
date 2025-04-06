cc519a0eef936f35c1ff4d62e0ca62b3798988e1
Haesung <koh1260@naver.com>
2025-04-06 21:43:17 +0900

    feat: 버튼 컴포넌트 disable, submit type prop 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -5,6 +5,8 @@ interface ButtonProps {
   width: string;
   color: "yellow";
   onClick: () => void;
+  disabled?: boolean;
+  isSubmitType?: boolean;
   isActive?: boolean;
   fontSize?: FontSize;
   className?: string;
@@ -18,9 +20,12 @@ export default function Button({
   title,
   color,
   onClick,
+  disabled = false,
+  isSubmitType = false,
   isActive,
   width,
   fontSize = "text-base",
+  className,
 }: ButtonProps) {
   const styles = isActive
     ? "bg-yellowPressedBtn pb-1"
@@ -28,13 +33,15 @@ export default function Button({
 
   return (
     <button
-      className={`${styles} ${fontSize} bg-cover bg-no-repeat flex justify-center items-center`}
+      type={isSubmitType ? "submit" : "button"}
+      className={`${styles} ${fontSize} bg-cover bg-no-repeat flex justify-center items-center ${className}`}
       style={{
         aspectRatio: "3/1",
         width,
         height: "auto",
       }}
       onClick={onClick}
+      disabled={disabled}
     >
       <p>{title}</p>
     </button>
