847d91fa44bbe001fcab82f75e47f40b93509242
Haesung <koh1260@naver.com>
2025-04-12 02:45:39 +0900

    feat: 회원 검색 api

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,6 +1,6 @@
 import { http } from "@/api/http";
 import { Provider } from "@/types/client/unions";
-import { GetUserResponse } from "mmorn-type";
+import { GetUserResponse, SearchUserResponse } from "mmorn-type";
 
 export interface NotRegisteredUserResponse {
   readonly message: string;
@@ -24,5 +24,19 @@ export interface AdditionalRegisterData {
 
 export async function getMyProfile(): Promise<GetUserResponse> {
   const response = await http.get<GetUserResponse>(`/users/my`);
+  return response.data;
+}
+
+// TODO 타입 배포되면 대치
+interface SearchUserRequest {
+  readonly search: string;
+  readonly varient: "NICKNAME" | "TAG";
+  readonly cursor?: string;
+  readonly limit?: number;
+}
+export async function searchUsers(query: SearchUserRequest) {
+  const response = await http.get<SearchUserResponse>("/users/search", {
+    params: query,
+  });
   return response.data;
 }
