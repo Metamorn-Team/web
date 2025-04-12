8c3b05ef626afff665acf5f70d41d8ef7f0150db
Haesung <koh1260@naver.com>
2025-04-12 17:41:50 +0900

    refactor: 기존 소켓 타입 제거, mmorntype으로 typed socket 타입 생성

--- src/types/socket-io/index.ts
+++ src/types/socket-io/index.ts
@@ -1,22 +1,4 @@
-import { UserInfo } from "@/types/socket-io/response";
+import { ClientToServer, ServerToClient } from "mmorntype";
+import { Socket } from "socket.io-client";
 
-export interface ServerToClientEvents {
-  playerJoin: (data: UserInfo & { x: number; y: number }) => void;
-  playerLeft: (data: { id: string }) => void;
-  playerMoved: (data: UserInfo & { x: number; y: number }) => void;
-  activeUsers: (activeUsers: (UserInfo & { x: number; y: number })[]) => void;
-}
-
-export interface ClientToServerEvents {
-  playerJoin: (data: { type?: "dev" | "design"; x: number; y: number }) => void;
-  playerMoved: (data: { x: number; y: number }) => void;
-}
-
-export interface InterServerEvents {
-  pong: () => void;
-}
-
-export interface SocketData {
-  name: string;
-  age: number;
-}
+export type TypedSocket = Socket<ServerToClient, ClientToServer>;
