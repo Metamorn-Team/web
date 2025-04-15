df53b951db0dcd9fd645a3ddcba622a4838c418c
Haesung <koh1260@naver.com>
2025-04-16 03:26:19 +0900

    refactor: input manager 개편

--- src/game/managers/input-manager.ts
+++ src/game/managers/input-manager.ts
@@ -3,44 +3,77 @@ import { Keys } from "@/types/game/enum/key";
 
 export class InputManager {
   private keyMap: Map<Keys, Phaser.Input.Keyboard.Key> = new Map();
+  private scene: Phaser.Scene;
 
   constructor(scene: Phaser.Scene) {
-    console.log(scene);
-    if (scene.input.keyboard) {
-      this.keyMap.set(
-        Keys.UP,
-        scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP)
-      );
-      this.keyMap.set(
-        Keys.DOWN,
-        scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN)
-      );
-      this.keyMap.set(
-        Keys.LEFT,
-        scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT)
-      );
-      this.keyMap.set(
-        Keys.RIGHT,
-        scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT)
-      );
+    this.scene = scene;
 
-      this.keyMap.set(
-        Keys.SPACE,
-        scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
-      );
+    if (!scene.input.keyboard) {
+      console.warn("Keyboard input is not enabled in this scene!");
+      return;
     }
+
+    this.registerKey(
+      Keys.UP,
+      scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP)
+    );
+    this.registerKey(
+      Keys.DOWN,
+      scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN)
+    );
+    this.registerKey(
+      Keys.LEFT,
+      scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT)
+    );
+    this.registerKey(
+      Keys.RIGHT,
+      scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT)
+    );
+    this.registerKey(
+      Keys.SPACE,
+      scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
+    );
+    this.registerKey(
+      Keys.ENTER,
+      scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER)
+    );
+  }
+
+  registerKey(keyEnum: Keys, keyCode: Phaser.Input.Keyboard.Key): void {
+    if (!this.scene.input.keyboard) return;
+    this.keyMap.set(keyEnum, this.scene.input.keyboard.addKey(keyCode));
   }
 
   getPressedKeys(): Keys[] {
     const keys: Keys[] = [];
 
-    if (this.keyMap.get(Keys.UP)!.isDown) keys.push(Keys.UP);
-    if (this.keyMap.get(Keys.DOWN)!.isDown) keys.push(Keys.DOWN);
-    if (this.keyMap.get(Keys.LEFT)!.isDown) keys.push(Keys.LEFT);
-    if (this.keyMap.get(Keys.RIGHT)!.isDown) keys.push(Keys.RIGHT);
-    if (Phaser.Input.Keyboard.JustDown(this.keyMap.get(Keys.SPACE)!))
-      keys.push(Keys.SPACE);
+    this.keyMap.forEach((key, keyEnum) => {
+      if (key.isDown) {
+        keys.push(keyEnum);
+      }
+    });
 
     return keys;
+  }
+
+  getJustDownKeys(): Keys[] {
+    const keys: Keys[] = [];
+
+    this.keyMap.forEach((key, keyEnum) => {
+      if (Phaser.Input.Keyboard.JustDown(key)) {
+        keys.push(keyEnum);
+      }
+    });
+
+    return keys;
+  }
+
+  isKeyDown(key: Keys): boolean {
+    return this.keyMap.get(key)?.isDown || false;
+  }
+
+  isKeyJustDown(key: Keys): boolean {
+    const phaserKey = this.keyMap.get(key);
+    return phaserKey ? Phaser.Input.Keyboard.JustDown(phaserKey) : false;
   }
 }
--- src/types/game/enum/key.ts
+++ src/types/game/enum/key.ts
@@ -4,4 +4,5 @@ export enum Keys {
   LEFT,
   RIGHT,
   SPACE,
+  ENTER,
 }
