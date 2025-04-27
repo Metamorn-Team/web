39105b284a0f40f1c8b61d3d6e0f8168007cd618
Haesung <koh1260@naver.com>
2025-04-27 18:05:20 +0900

    feat: 상품 타입 속성 추가

--- src/types/client/product.ts
+++ src/types/client/product.ts
@@ -1,3 +1,6 @@
+import { ItemGrade } from "mmorntype/dist/src/domain/types/item.types";
+import { PurchasedStatus } from "mmorntype/dist/src/domain/types/product.types";
+
 export interface Product {
   id: string;
   name: string;
@@ -6,6 +9,8 @@ export interface Product {
   price: number;
   type: string;
   key: string;
+  grade: ItemGrade;
+  purchasedStatus: PurchasedStatus;
 }
 
 export type EquippedItem = Pick<Product, "id" | "name" | "price">;
