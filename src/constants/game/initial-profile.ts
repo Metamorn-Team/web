ddf40b1f768d43d600c5dd22ab3b6933061af1af
Haesung <koh1260@naver.com>
2025-04-28 03:29:21 +0900

    chore: 기본 프로필에 닉네임 제거

--- src/constants/game/initial-profile.ts
+++ src/constants/game/initial-profile.ts
@@ -3,6 +3,6 @@ import { PAWN } from "@/constants/game/entities";
 export const INITIAL_PROFILE = {
   id: "metamorn",
   avatarKey: PAWN("purple"),
-  nickname: "메타몬 뉴비",
+  nickname: " ",
   tag: "metamorn",
 };
