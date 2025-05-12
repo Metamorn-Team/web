f8e3b70302755550edd1424ef1a5c64c9ce003cf
Haesung <koh1260@naver.com>
2025-05-13 01:31:00 +0900

    feat: 세선 스토리지 타입 추가

--- src/types/storage/session-storage.ts
+++ src/types/storage/session-storage.ts
@@ -4,6 +4,6 @@ export interface SessionStorageData {
   current_scene: string;
   current_island_id: string;
   current_island_name: string;
-  current_island_type: string;
+  current_island_type: "NORMAL" | "DESERTED";
   zone_type: ZoneType;
 }
