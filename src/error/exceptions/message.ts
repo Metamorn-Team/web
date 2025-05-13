0fa084229852f332e5e98cf431d99d57325d440f
Haesung <koh1260@naver.com>
2025-05-14 03:08:18 +0900

    feat: 에러 메세지 상수로 관리

---
+++ src/error/exceptions/message.ts
@@ -0,0 +1,5 @@
+export const ISLAND_FULL_MESSAGE = "섬에 자리가 없어요..";
+export const ISLANF_NOT_FOUND_MESSAGE = "이미 사라진 섬이에요..";
+
+export const UNKNOWN_MESSAGE = "잠시후 다시 시도해주세요..";
+export const CONFLIC_MESSAGE = "이미 처리 중인 작업이에요..";
