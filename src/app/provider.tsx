28712a21c497c574c9efd31cb88f4f6b869a2a0e
Haesung <koh1260@naver.com>
2025-05-15 19:53:21 +0900

    fix: query 요청 재시도 안 하도록 변경

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -5,7 +5,16 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
-  const [queryClient] = React.useState(() => new QueryClient());
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: false,
+          },
+        },
+      })
+  );
 
   return (
     <QueryClientProvider client={queryClient}>
