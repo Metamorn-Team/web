b6c15db83adc902967142e9877ee486c6a553a85
Haesung <koh1260@naver.com>
2025-05-17 01:21:47 +0900

    feat: 회원 프로필 조회 enabled 매개변수 추가

--- src/hook/queries/useGetUserProfile.ts
+++ src/hook/queries/useGetUserProfile.ts
@@ -3,9 +3,10 @@ import { useQuery } from "@tanstack/react-query";
 
 export const QUERY_KEY = "user-profile";
 
-export const useGetUserProfile = (userId: string) => {
+export const useGetUserProfile = (userId: string, enabled: boolean = true) => {
   return useQuery({
     queryKey: [QUERY_KEY, userId],
     queryFn: () => getProfile(userId),
+    enabled,
   });
 };
