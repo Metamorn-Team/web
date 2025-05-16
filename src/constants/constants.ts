6ac41d82a22a9242658c87d4343d5bcd14673ef1
Haesung <koh1260@naver.com>
2025-05-17 04:28:56 +0900

    .

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,3 @@
-// export const API_BASE_URL = "https://dev-api.livisland.com";
-export const API_BASE_URL = "http://localhost:4000";
+export const API_BASE_URL = "https://dev-api.livisland.com";
+// export const API_BASE_URL = "http://localhost:4000";
 export const CDN_BASE_URL = "https://cdn.metamorn.com";
