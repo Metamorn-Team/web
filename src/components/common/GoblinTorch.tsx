453e52356275ff6752e2fe97c06eac9315586c46
Haesung <koh1260@naver.com>
2025-05-20 03:32:24 +0900

    chore: 고블린 이미지 cdn 주소 사용

--- src/components/common/GoblinTorch.tsx
+++ src/components/common/GoblinTorch.tsx
@@ -1,9 +1,13 @@
+import { CDN_BASE_URL } from "@/constants/constants";
+import { TORCH_GOBLIN } from "@/constants/game/entities";
 import React from "react";
 
 const GoblinTorch = ({ className }: { className?: string }) => {
   return (
     <div
-      className={`w-60 h-60 bg-[url('/game/enemy/red_torch_goblin.png')] bg-[length:700%_500%] animate-fire ${className}`}
+      className={`w-60 h-60 bg-[url(${CDN_BASE_URL}/sprite/${TORCH_GOBLIN(
+        "red"
+      )}.png)] bg-[length:700%_500%] animate-fire ${className}`}
     />
   );
 };
