552ab022876f3e2e438730b64312059c3a5601a0
Haesung <koh1260@naver.com>
2025-06-26 23:30:44 +0900

    chore: 상점 메타 태그 추가

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -4,6 +4,8 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "🎁 리아 상점",
   description: "귀여운 아이템을 골라보세요!",
+  keywords: ["리아", "메타버스", "픽셀", "아이템", "상점", "리브아일랜드"],
+  robots: "index, follow",
 };
 
 export default function StorePage() {
