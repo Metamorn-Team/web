9baa2a9c0c38fb39972eefdcf663d2c7e2f982bf
Haesung <koh1260@naver.com>
2025-07-04 19:33:33 +0900

    chore: 쿠키 설정

--- src/utils/device-id.ts
+++ src/utils/device-id.ts
@@ -10,6 +10,12 @@ export function setDeviceId() {
   if (existing) return existing.split("=")[1];
 
   const newId = v4();
-  document.cookie = `${cookieName}=${newId}; path=/`;
+  const isDev =
+    location.hostname === "localhost" || location.hostname === "127.0.0.1";
+
+  document.cookie = `${cookieName}=${newId}; path=/; domain=${
+    isDev ? "" : ".livisland.com"
+  } ${isDev ? "" : "; SameSite=None; Secure"}`;
+  // document.cookie = `${cookieName}=${newId}; path=/`;
   return newId;
 }
