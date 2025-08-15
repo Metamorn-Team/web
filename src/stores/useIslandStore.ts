3f489829c58f760e2622022cde52a0cb8e7f3064
Haesung <koh1260@naver.com>
2025-08-15 20:32:04 +0900

    feat: 섬 정보 store

--- src/stores/useIslandStore.ts
+++ src/stores/useIslandStore.ts
@@ -3,16 +3,23 @@ import { create } from "zustand";
 
 interface IslandState {
   id?: string;
+  type?: "NORMAL" | "DESERTED" | "PRIVATE";
+  password?: string;
 }
 
 interface Action {
-  setIsland: (id: string) => void;
+  setIsland: (
+    id: string,
+    type?: "NORMAL" | "DESERTED" | "PRIVATE",
+    password?: string
+  ) => void;
   clear: () => void;
 }
 
 export const useIslandStore = create<IslandState & Action>((set) => ({
   id: undefined,
   type: undefined,
-  setIsland: (id) => set({ id }),
-  clear: () => set({ id: undefined }),
+  password: undefined,
+  setIsland: (id, type, password) => set({ id, type, password }),
+  clear: () => set({ id: undefined, type: undefined, password: undefined }),
 }));
