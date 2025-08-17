6cf18a46c9d001d75fe6f15b0b91806dc48a100c
Haesung <koh1260@naver.com>
2025-08-17 23:31:05 +0900

    feat: 에러 폴백 prop 추가

--- src/components/common/ErrorFallback.tsx
+++ src/components/common/ErrorFallback.tsx
@@ -6,6 +6,8 @@ interface ErrorFallbackProps {
   size?: "s" | "m" | "l";
   onClick?: () => void;
   buttonText?: string;
+  backgroundColor?: string;
+  fullScreen?: boolean;
 }
 
 export default function ErrorFallback({
@@ -13,32 +15,45 @@ export default function ErrorFallback({
   size = "s",
   onClick,
   buttonText,
+  backgroundColor = "bg-gray-200",
+  fullScreen = true,
 }: ErrorFallbackProps) {
   const sizeMap = {
     s: {
       pawn: "w-[60px] h-[60px]",
       font: "text-sm",
+      left: "left-[10px]",
     },
     m: {
       pawn: "w-[80px] h-[80px]",
       font: "text-base",
+      left: "left-[15px]",
     },
     l: {
       pawn: "w-[100px] h-[100px]",
       font: "text-lg",
+      left: "left-[20px]",
     },
   };
 
   return (
-    <div className="absolute inset-0 flex flex-col gap-2 items-center justify-center bg-gray-200">
-      <div className="relative flex flex-col items-center text-gray-500 gap-2">
-        <Pawn
-          color={getRandomPawnColor()}
-          animation="run"
-          className={sizeMap[size].pawn}
-          paused
-        />
-        <div className="absolute left-[50px] top-0 text-2xl">💧</div>
+    <div
+      className={`${
+        fullScreen ? "absolute inset-0" : ""
+      } flex flex-col gap-2 items-center justify-center ${backgroundColor}`}
+    >
+      <div className="flex flex-col items-center text-gray-500 gap-2">
+        <div className="relative">
+          <Pawn
+            color={getRandomPawnColor()}
+            animation="run"
+            className={sizeMap[size].pawn}
+            paused
+          />
+          <div className={`absolute ${sizeMap[size].left} top-0 text-2xl`}>
+            💧
+          </div>
+        </div>
         <div className={sizeMap[size].font}>{message}</div>
       </div>
       {buttonText && (
